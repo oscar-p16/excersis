0e5e836410b0d5a313d7fa35e0e496f2b830acf2
oscar p <oscarperezm16@hotmail.com>
2021-05-29 15:18:11 -0500

    gigasecond

---
+++ gigasecond/gigasecond.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = () => {
+  var input_time = input_date.getTime(); 
+  var giga_second = 1000000000; 
+  var giga_date = new Date(input_time + (giga_second * 1000)); 
+  return giga_date;
+};
